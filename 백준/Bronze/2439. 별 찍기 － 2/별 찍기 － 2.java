2ec3652c4a8ec78b0f658f7fb0cd778fb16c5c89
JunnnMo3o <83547182+JUNSUNG-KIM2546@users.noreply.github.com>
2023-11-18 16:53:29 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//인풋
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	//아웃풋
+		String s = br.readLine();
+		String d = " ";
+		String st = "*";
+		
+		int co = Integer.parseInt(s);	// 스트링을 인트로 변환
+		for(int i=1; i<=co; i++) {
+			bw.write(d.repeat(co - i));
+			for(int j=0; j<i; j++) {
+				bw.write(st);	//할당
+			}
+			bw.newLine();	// 개행 줄 추가
+		}
+		bw.flush(); //출력
+		bw.close(); //종료
+	}
+}
